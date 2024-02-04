9deece6a20f25951ddc2dcf9d4ff473c156d6034
Sumit <akoliyasumit@hotmail.com>
2024-02-04 21:50:17 +0000

    permitting GET method for users/status

--- src/main/java/online/sumitakoliya/photoapp/api/users/security/WebSecurity.java
+++ src/main/java/online/sumitakoliya/photoapp/api/users/security/WebSecurity.java
@@ -129,6 +129,7 @@ public class WebSecurity {
   
     	http.authorizeHttpRequests((authz) -> authz
         .requestMatchers(HttpMethod.POST, "/users").permitAll()
+        .requestMatchers(HttpMethod.GET, "/users/status").permitAll()
         .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll())
         .addFilter(authenticationFilter)
         .authenticationManager(authenticationManager)
