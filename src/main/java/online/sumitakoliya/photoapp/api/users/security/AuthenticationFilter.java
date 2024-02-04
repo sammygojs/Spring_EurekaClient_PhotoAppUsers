8a1b87a3116cafe5b1adac45cef0b3606b315d2c
Sumit <akoliyasumit@hotmail.com>
2024-02-04 21:44:18 +0000

    working user login and token fetching with status 200

--- src/main/java/online/sumitakoliya/photoapp/api/users/security/AuthenticationFilter.java
+++ src/main/java/online/sumitakoliya/photoapp/api/users/security/AuthenticationFilter.java
@@ -1,8 +1,85 @@
-package online.sumitakoliya.photoapp.api.users.security;
+//package online.sumitakoliya.photoapp.api.users.security;
+//
+//import online.sumitakoliya.photoapp.api.users.service.UsersService;
+//import online.sumitakoliya.photoapp.api.users.shared.UserDto;
+//import online.sumitakoliya.photoapp.api.users.ui.model.LoginRequestModel;
+//
+//import java.io.IOException;
+//import java.time.Instant;
+//import java.util.ArrayList;
+//import java.util.Base64;
+//import java.util.Date;
+//
+//import javax.crypto.*;
+//import javax.crypto.spec.SecretKeySpec;
+//import org.springframework.core.env.Environment;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.AuthenticationException;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//
+//import io.jsonwebtoken.Jwts;
+//
+//import org.springframework.security.core.userdetails.User;
+//import jakarta.servlet.FilterChain;
+//import jakarta.servlet.ServletException;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//
+//public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+//	
+//	private UsersService usersService;
+//	private Environment env;
+//
+//	public Authentication attemptAuthentication(HttpServletRequest req, HttpServletResponse res)
+//			throws AuthenticationException {
+//		try {
+//			LoginRequestModel creds = new ObjectMapper().readValue(req.getInputStream(), LoginRequestModel.class);
+//
+//			return getAuthenticationManager().authenticate(
+//					new UsernamePasswordAuthenticationToken(creds.getEmail(), creds.getPassword(), new ArrayList<>()));
+//		} catch (IOException e) {
+//			throw new RuntimeException(e);
+//		}
+//	}
+//
+//	public AuthenticationFilter(AuthenticationManager authenticationManager, UsersService usersService, Environment env) {
+//		super(authenticationManager);
+//		this.usersService = usersService;
+//		this.env = env;
+//	}
+//	
+//	@Override
+//	protected void successfulAuthentication(HttpServletRequest req, HttpServletResponse res, 
+//			FilterChain chain, Authentication auth) throws IOException, ServletException{
+//		
+//		String userName = ((User)auth.getPrincipal()).getUsername();
+//		UserDto userDetails = usersService.getUserDetailsByEmail(userName);
+//		String tokenSecret = env.getProperty("token.secret");
+//		byte[] secretKeyBytes = Base64.getEncoder().encode(tokenSecret.getBytes());
+//		SecretKey secretKey = new SecretKeySpec(secretKeyBytes, "HmacSHA512");
+//		
+//		Instant now = Instant.now();
+//		String token = Jwts.builder()
+//		.subject(userDetails.getUserId().toString())
+//		.expiration(Date.from(now.plusMillis(Long.parseLong(env.getProperty("token.expiration_time")))))
+//		.issuedAt(Date.from(now))
+//		.signWith(secretKey)
+//		.compact();
+////		.setSubject(userDetails.getUserId().toString())
+//		
+//		res.addHeader("token", token);
+//		res.addHeader("userId", userDetails.getUserId().toString());
+//		
+//		
+//	}
+//	
+//}
 
-import online.sumitakoliya.photoapp.api.users.service.UsersService;
-import online.sumitakoliya.photoapp.api.users.shared.UserDto;
-import online.sumitakoliya.photoapp.api.users.ui.model.LoginRequestModel;
+package online.sumitakoliya.photoapp.api.users.security;
 
 import java.io.IOException;
 import java.time.Instant;
@@ -10,71 +87,76 @@ import java.util.ArrayList;
 import java.util.Base64;
 import java.util.Date;
 
-import javax.crypto.*;
+import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
+
 import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.core.userdetails.User;
 
+import online.sumitakoliya.photoapp.api.users.service.UsersService;
+import online.sumitakoliya.photoapp.api.users.shared.UserDto;
+import online.sumitakoliya.photoapp.api.users.ui.model.LoginRequestModel;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.jsonwebtoken.Jwts;
-
-import org.springframework.security.core.userdetails.User;
+import io.jsonwebtoken.SignatureAlgorithm;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
-	
-	private UsersService usersService;
-	private Environment env;
 
+	private UsersService usersService;
+	private Environment environment;
+
+	public AuthenticationFilter(UsersService usersService, Environment environment,
+			AuthenticationManager authenticationManager) {
+		super(authenticationManager);
+		this.usersService = usersService;
+		this.environment = environment;
+	}
+
+	@Override
 	public Authentication attemptAuthentication(HttpServletRequest req, HttpServletResponse res)
 			throws AuthenticationException {
 		try {
+
 			LoginRequestModel creds = new ObjectMapper().readValue(req.getInputStream(), LoginRequestModel.class);
 
 			return getAuthenticationManager().authenticate(
 					new UsernamePasswordAuthenticationToken(creds.getEmail(), creds.getPassword(), new ArrayList<>()));
+
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
 	}
 
-	public AuthenticationFilter(AuthenticationManager authenticationManager, UsersService usersService, Environment env) {
-		super(authenticationManager);
-		this.usersService = usersService;
-		this.env = env;
-	}
-	
+	@SuppressWarnings("deprecation")
 	@Override
-	protected void successfulAuthentication(HttpServletRequest req, HttpServletResponse res, 
-			FilterChain chain, Authentication auth) throws IOException, ServletException{
-		
-		String userName = ((User)auth.getPrincipal()).getUsername();
+	protected void successfulAuthentication(HttpServletRequest req, HttpServletResponse res, FilterChain chain,
+			Authentication auth) throws IOException, ServletException {
+
+		String userName = ((User) auth.getPrincipal()).getUsername();
 		UserDto userDetails = usersService.getUserDetailsByEmail(userName);
-		String tokenSecret = env.getProperty("token.secret");
+		String tokenSecret = environment.getProperty("token.secret");
 		byte[] secretKeyBytes = Base64.getEncoder().encode(tokenSecret.getBytes());
-		SecretKey secretKey = new SecretKeySpec(secretKeyBytes, "HmacSHA512");
-		
+		SecretKey secretKey = new SecretKeySpec(secretKeyBytes, SignatureAlgorithm.HS512.getJcaName());
+
 		Instant now = Instant.now();
-		String token = Jwts.builder()
-		.subject(userDetails.getUserId().toString())
-		.expiration(Date.from(now.plusMillis(Long.parseLong(env.getProperty("token.expiration_time")))))
-		.issuedAt(Date.from(now))
-		.signWith(secretKey)
-		.compact();
-//		.setSubject(userDetails.getUserId().toString())
-		
+
+		String token = Jwts.builder().setSubject(userDetails.getUserId().toString())
+				.setExpiration(
+						Date.from(now.plusMillis(Long.parseLong(environment.getProperty("token.expiration_time")))))
+				.setIssuedAt(Date.from(now)).signWith(secretKey, SignatureAlgorithm.HS512).compact();
+
 		res.addHeader("token", token);
 		res.addHeader("userId", userDetails.getUserId().toString());
-		
-		
 	}
-	
 }
+
--- src/main/java/online/sumitakoliya/photoapp/api/users/security/WebSecurity.java
+++ src/main/java/online/sumitakoliya/photoapp/api/users/security/WebSecurity.java
@@ -1,7 +1,85 @@
+//package online.sumitakoliya.photoapp.api.users.security;
+//
+//import org.springframework.context.annotation.Bean;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.env.Environment;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+//
+//import online.sumitakoliya.photoapp.api.users.service.UsersService;
+//
+//import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class WebSecurity {
+//	
+//	private Environment env;
+//	
+//	private UsersService usersService;
+//	private BCryptPasswordEncoder bCryptPasswordEncoder;
+//	
+////	public WebSecurity(Environment env) {
+////		this.env = env;
+////	}
+//	
+//	
+//	public WebSecurity(Environment env, UsersService usersService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+//		this.env = env;
+//		this.usersService = usersService;
+//		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+//	}
+//
+//
+//	@Bean
+//	protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+//		
+//		//configure authenticationManagereBuilder
+//		AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+//		
+//		authenticationManagerBuilder.userDetailsService(usersService).passwordEncoder(bCryptPasswordEncoder);
+//		
+//		AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+//		
+////		http.csrf().disable();
+////		http.cs
+//		http.csrf((csrf)->csrf.disable());
+//		
+//		
+////		http.aut
+//		http.
+//		authorizeHttpRequests((authorizeHttpRequests)->authorizeHttpRequests
+//				.requestMatchers(HttpMethod.POST, "/users").access(new WebExpressionAuthorizationManager("hasIpAddress('"+env.getProperty("gateway.ip")+"')"))
+////				.permitAll()
+//				.requestMatchers(HttpMethod.GET, "/users/status").access(new WebExpressionAuthorizationManager("hasIpAddress('"+env.getProperty("gateway.ip")+"')"))
+//				.requestMatchers(new AntPathRequestMatcher("/h2console/**")).permitAll()
+//				);
+//		
+//		http.addFilter(new AuthenticationFilter( authenticationManager,usersService, env));
+//		http.authenticationManager(authenticationManager);
+//		
+//		http.sessionManagement((sm)->sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+//				
+//		
+//		
+////		http.headers().frameOptions().disable();
+//		http.headers((headers)->headers.frameOptions((f)->f.disable()));
+//		
+//		return http.build();
+//	}
+//}
+
 package online.sumitakoliya.photoapp.api.users.security;
 
 import org.springframework.context.annotation.Bean;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
@@ -16,63 +94,50 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import online.sumitakoliya.photoapp.api.users.service.UsersService;
 
-import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
-
 @Configuration
 @EnableWebSecurity
 public class WebSecurity {
 	
-	private Environment env;
-	
+	private Environment environment;
 	private UsersService usersService;
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
 	
-//	public WebSecurity(Environment env) {
-//		this.env = env;
-//	}
-	
-	
-	public WebSecurity(Environment env, UsersService usersService, BCryptPasswordEncoder bCryptPasswordEncoder) {
-		this.env = env;
+	public WebSecurity(Environment environment, UsersService usersService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+		this.environment = environment;
 		this.usersService = usersService;
 		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
 	}
+    
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+    	
+    	// Configure AuthenticationManagerBuilder
+    	AuthenticationManagerBuilder authenticationManagerBuilder = 
+    			http.getSharedObject(AuthenticationManagerBuilder.class);
+    	
+    	authenticationManagerBuilder.userDetailsService(usersService)
+    	.passwordEncoder(bCryptPasswordEncoder);
+    	
+    	AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+    	
+    	// Create AuthenticationFilter
+    	AuthenticationFilter authenticationFilter = 
+    			new AuthenticationFilter(usersService, environment, authenticationManager);
+    	authenticationFilter.setFilterProcessesUrl(environment.getProperty("login.url.path"));
+    	
+    	http.csrf((csrf) -> csrf.disable());
+  
+    	http.authorizeHttpRequests((authz) -> authz
+        .requestMatchers(HttpMethod.POST, "/users").permitAll()
+        .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll())
+        .addFilter(authenticationFilter)
+        .authenticationManager(authenticationManager)
+        .sessionManagement((session) -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+ 
+    	http.headers((headers) -> headers.frameOptions((frameOptions) -> frameOptions.sameOrigin()));
+        return http.build();
 
-
-	@Bean
-	protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
-		
-		//configure authenticationManagereBuilder
-		AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
-		
-		authenticationManagerBuilder.userDetailsService(usersService).passwordEncoder(bCryptPasswordEncoder);
-		
-		AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
-		
-//		http.csrf().disable();
-//		http.cs
-		http.csrf((csrf)->csrf.disable());
-		
-		
-//		http.aut
-		http.
-		authorizeHttpRequests((authorizeHttpRequests)->authorizeHttpRequests
-				.requestMatchers(HttpMethod.POST, "/users").access(new WebExpressionAuthorizationManager("hasIpAddress('"+env.getProperty("gateway.ip")+"')"))
-//				.permitAll()
-				.requestMatchers(HttpMethod.GET, "/users/status").access(new WebExpressionAuthorizationManager("hasIpAddress('"+env.getProperty("gateway.ip")+"')"))
-				.requestMatchers(new AntPathRequestMatcher("/h2console/**")).permitAll()
-				);
-		
-		http.addFilter(new AuthenticationFilter( authenticationManager,usersService, env));
-		http.authenticationManager(authenticationManager);
-		
-		http.sessionManagement((sm)->sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-				
-		
-		
-//		http.headers().frameOptions().disable();
-		http.headers((headers)->headers.frameOptions((f)->f.disable()));
-		
-		return http.build();
-	}
+    }
 }
+
